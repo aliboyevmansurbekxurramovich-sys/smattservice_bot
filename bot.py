df5bc5cb6d5d20d5a6a51be8f96508dd4cea16a2
aliboyevmansurbekxurramovich-sys <aliboyev.mansurbek.xurramovich@gmail.com>
2026-07-15 21:40:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -337,6 +337,8 @@ async def main():
     logger.info("Bot ishga tushdi!")
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("admin_qosh", admin_qosh_command))
+    app.add_handler(CommandHandler("adminlar", admin_list_command))
     app.add_handler(CommandHandler("xarajat", xarajat_command))
     app.add_handler(CallbackQueryHandler(callback_handler))
     app.add_handler(MessageHandler(filters.CONTACT, contact_handler))
